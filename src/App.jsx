d2e317e1db3a9d2dc4bd195cfa118b3217ec4ca2
nipunnishamaheeka <nishamaheeka123@gmail.com>
2024-09-26 12:25:15 +0530

    added chatApp components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import List from "./components/list/list";
+import Chat from "./components/chat/chat";
+import Detail from "./components/details/Detail";
+
 const App = () => {
   return (
-    <div className=''>App</div>
-  )
-}
+    <div className="container">
+      <List />
+      <Chat />
+      <Detail />
+    </div>
+  );
+};
 
-export default App
+export default App;
