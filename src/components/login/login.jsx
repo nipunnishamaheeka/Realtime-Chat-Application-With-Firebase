1149d4cf64116f59c800f47c3112db9035b06bfd
nipunnishamaheeka <nishamaheeka123@gmail.com>
2024-09-30 17:23:38 +0530

    signup Email and password firebase

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import "./login.css";
 import { toast } from "react-toastify";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../lib/firebase";
 
 const Login = () => {
   const [avatar, setAvatar] = useState({
@@ -15,12 +17,18 @@ const Login = () => {
       })
     }
   }
-  const handleRegister = (e) => {
+  const handleRegister = async (e) => {
     e.preventDefault()
     const formData = new FormData(e.target)
 
     const { username, email, password } = Object.fromEntries(formData);
-    console.log(username)
+    try {
+      
+    const res = await createUserWithEmailAndPassword(auth, email, password)
+    } catch (err) {
+      console.error(err)
+      toast.error(err.message)
+  }
   }
   const handleLogin = (e) => {
     e.preventDefault()
