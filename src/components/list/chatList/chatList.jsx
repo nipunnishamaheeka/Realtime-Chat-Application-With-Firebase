bba20ff4d4aad9f6a4562d24a0ac50994e1edd05
nipunnishamaheeka <nishamaheeka123@gmail.com>
2024-09-30 13:49:48 +0530

    add addUser component

--- src/components/list/chatList/chatList.jsx
+++ src/components/list/chatList/chatList.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./chatList.css";
+import AddUser from "./addUser/addUser";
 
 const ChatList = () => {
   const [addMode, setAddMode] = useState(false);
@@ -52,6 +53,7 @@ const ChatList = () => {
           <p>Hello Doe</p>
         </div>
       </div>
+  {addMode && <AddUser/>}
     </div>
   );
 };
