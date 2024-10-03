9cc289dc19ec857a4d7ed6486d2c1d997de06a84
nipunnishamaheeka <nishamaheeka123@gmail.com>
2024-10-03 09:50:29 +0530

    addUser component to search and display user information

--- src/components/list/chatList/addUser/addUser.jsx
+++ src/components/list/chatList/addUser/addUser.jsx
@@ -1,21 +1,50 @@
-import "./addUser.css"
+import { useState } from "react";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import "./addUser.css";
+import { db } from "../../../../lib/firebase";
 
 const AddUser = () => {
-    return (
-        <div className="addUser">
-            <form action="">
-                <input type="text" placeholder="Username" name="username" />
-                <button>Search</button>
-            </form>
-            <div className="user">
-                <div className="detail">
-                    <img src="./avatar.png" alt="" />
-                    <span>Nipun Nishamaheeka</span>
-                </div>
-                <button>Add User</button>
-            </div>
-        </div>
-    )
-}
+  const [user, setUser] = useState(null);
 
-export default AddUser
+  const handleSearch = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const username = formData.get("username");
+
+    try {
+      const userRef = collection(db, "users");
+
+      const q = query(userRef, where("username", "==", username));
+      const querySnapshot = await getDocs(q);
+
+      if (!querySnapshot.empty) {
+        setUser(querySnapshot.docs[0].data());
+      } else {
+        setUser(null);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="addUser">
+      <form onSubmit={handleSearch}>
+        <input type="text" placeholder="Username" name="username" />
+        <button>Search</button>
+      </form>
+      {user && (
+        <div className="user">
+          <div className="detail">
+            <img src={user.avatar || "./avatar.png"} alt="User avatar" />
+            <span>{user.username}</span>
+          </div>
+          <button>Add User</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AddUser;
