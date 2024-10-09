d753900a377d73742a7d279e555a421d44a1c8ad
nipunnishamaheeka <nishamaheeka123@gmail.com>
2024-10-09 11:35:29 +0530

    chat component to fetch and display chat data from Firestore

--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -1,15 +1,34 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./chat.css";
 import EmojiPicker from "emoji-picker-react";
+import {
+  doc,
+  onSnapshot,
+} from "firebase/firestore";
+import { db } from "../../lib/firebase";
 
 const Chat = () => {
+  const [chat, setChat] = useState();
   const [open, setOpen] = useState(false);
   const [text, setText] = useState("");
 
   const endRef = useRef(null);
   useEffect(() => {
     endRef.current?.scrollIntoView({ behavior: "smooth" });
-  }, [])
+  }, []);
+
+  useEffect(() => {
+    const unSub = onSnapshot(
+      doc(db, "chats", "5dAR7KRcZHLmNHClkZxz"),
+      (res) => {
+        setChat(res.data());
+      }
+    );
+    return () => {
+      unSub();
+    };
+  }, []);
+  console.log(chat)
   const handleEmoji = (e) => {
     setText((prev) => prev + e.emoji);
     setOpen(false);
